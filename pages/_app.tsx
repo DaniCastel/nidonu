00224f68552ddafd21a41ce000cae171f91f1f9f
Daniela Castellanos <ldcastellanosa@gmail.com>
2023-09-13 18:59:09 -0500

    :heavy_plus_sign: Add NextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import * as React from "react";
+
 import { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
 
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 // tailwind.config.js
+const { nextui } = require("@nextui-org/react");
+
+// tailwind.config.js
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     // For the best performance and to avoid false positives,
     // be as specific as possible with your content configuration.
   ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
 };
